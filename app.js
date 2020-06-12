0d35438ad83f88d67824f617bfeec0703cd8b1ba
Kirankishore <39197993+Kirankishore17@users.noreply.github.com>
2020-06-12 18:37:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const mathSum = require('./math');
-console.log('Hello world ' + mathSum(2,2));
+// importing function from math.js
+// const mathSum = require('./math');
+// console.log('Hello world ' + mathSum(2,2));
+// if more than one function/variable mathSum.sum(1,2) mathSum.pi
+
+// event emitter 
+// const EventEmitter = require('events');
+// const eventEmitter = new EventEmitter();
+// eventEmitter.on('kk', () => {console.log('event emitter. !@')})
+// eventEmitter.emit('kk')
+
+// readLine module
