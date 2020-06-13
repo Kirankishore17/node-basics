80af552936bbc3fe4148d0840488e25f273d4343
Kirankishore <39197993+Kirankishore17@users.noreply.github.com>
2020-06-13 17:00:58 +0530

    http and express basics

--- app.js
+++ app.js
@@ -9,4 +9,31 @@
 // eventEmitter.on('kk', () => {console.log('event emitter. !@')})
 // eventEmitter.emit('kk')
 
-// readLine module
+// fs module
+// const fs = require('fs');
+// const readStream = fs.createReadStream('./ex1.txt', 'utf-8')
+// const writeStream = fs.createWriteStream('ex2.txt')
+//1. readStream.on('data', (ch) => writeStream.write(`// comments ${ch}`));
+//2. readStream.pipe(writeStream);
+
+// http module
+// const http = require('http');
+// const fs = require('fs')
+// http.createServer((req, res) => {
+//     const stream = fs.createReadStream('./ex.json')
+//     res.writeHead(200, {'Content-type':'application/json'})
+//     stream.pipe(res)
+// }).listen(3000);
+
+// Express 
+const express = require('express');
+const app = express();
+app.get('/', (req,res) => {
+    res.send('hello world')
+})
+app.get('/kk/:name', (req,res) => {
+    res.send('kk')
+    console.log(req.params)
+})
+app.listen(3000);
+
