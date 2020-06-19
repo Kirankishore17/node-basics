49e2797058981b72a7b64603f0b7fd8b2d4c81d7
Kirankishore <39197993+Kirankishore17@users.noreply.github.com>
2020-06-19 11:37:30 +0530

    Net ninja

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 // net ninja
 
-const http = require('http');
+// http
+// const http = require('http');
+// http.createServer((req, res) => {
+//     res.writeHead(200, {'Context-Type': 'Application/JSON'});
+//     var ob = {
+//         "name":"kiran"
+//     }
+//     res.end(JSON.stringify(ob));
+// }).listen(3000);
 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Context-Type': 'Application/JSON'});
-    var ob = {
-        "name":"kiran"
-    }
-    res.end(JSON.stringify(ob));
+// express get request
+// const express = require('express');
+// var app = express();
+// app.get("/", (req,res) => {
+//     res.send('jhdskcn');
+// })
+// app.listen(3000);
+
+// express body-parser
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+var body = bodyParser.json();
+
+app.post('/', (req,res) => {
+    console.log(req.url + "  " + req.json);
 }).listen(3000);
